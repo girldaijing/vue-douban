82c720a917bd14bcf6a9d108e361fc0a489bed10
girldaijing <2567553739@qq.com>
2018-12-18 18:01:28 +0800

    vuedouban

---
+++ precache-manifest.a13241850cb986edb1328f826524527e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "c5f0bacbf1a006cef1da",
+    "url": "/js/chunk-vendors.36296190.js"
+  },
+  {
+    "revision": "b1b5f63ffa20a779e2a7",
+    "url": "/js/chunk-151a3da4.fbaaece3.js"
+  },
+  {
+    "revision": "fef9bba2bea980552b9c",
+    "url": "/js/app.9c2ac5ac.js"
+  },
+  {
+    "revision": "7d67ccec67bd02fdf477b253b70cae08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1b5f63ffa20a779e2a7",
+    "url": "/css/chunk-151a3da4.540c74a9.css"
+  },
+  {
+    "revision": "fef9bba2bea980552b9c",
+    "url": "/css/app.08d785d2.css"
+  }
+];
